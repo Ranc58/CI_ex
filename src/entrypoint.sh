58199f061e91727c8807cb516a8650cec6a08ac7
Ranc58 <rancvova@gmail.com>
2018-08-06 16:53:36 +0300

    Add travis, some fixes

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-bash ./wait-for-it.sh postgres:5432
+bash ./wait-for-it.sh postgres:$DB_PORT -t 25
 alembic upgrade head
 python3 main.py
--- src/runtests.sh
+++ src/runtests.sh
@@ -2,5 +2,6 @@
 set -e
 
 export DB_NAME=test_$DB_NAME
+export PYTHONDONTWRITEBYTECODE=1
 
-python3 -m pytest tests/ -s -vv -ra --junitxml=./unit.xml --cov . --cov-report xml
+python3 -m pytest tests/ --ff --lf -s -vv -ra --junitxml=./unit.xml --cov . --cov-report xml
